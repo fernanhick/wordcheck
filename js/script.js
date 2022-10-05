a64278f20342a40fd8f7547a69502470ec697fa1
Fernando Hickman <fernanhick@gmail.com>
2022-10-05 19:19:30 +0100

    description added to word selected

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 import { words } from './data.js'
+import { wordlist } from './data.js'
 //const conflictWords = ['error', 'project', 'es']
-const conflictWords = words
+const conflictWords = wordlist
 let resultWords = new Set()
 
 const wordsList = document.querySelector('.words-list')
@@ -19,7 +20,7 @@ const declareBtnClose = () => {
 const populateWrapper = () => {
     wordsList.innerHTML = ''
     conflictWords.map((e) => {
-        wordsList.innerHTML += `<div class='word'><p>${e}</p><span class='close-btn'>x</span></div>`
+        wordsList.innerHTML += `<div class='word'><p>${e.word}</p><span class='close-btn'>x</span></div>`
     })
     declareBtnClose()
 }
@@ -44,7 +45,7 @@ const checkWords = (files) => {
             var original_string = content
 
             conflictWords.forEach((word) => {
-                let reg = new RegExp(word, 'gi')
+                let reg = new RegExp(word.word, 'gi')
                 original_string = original_string.replace(reg, (e) => {
                     resultWords.add(e)
                     return `<span class="highlight" id='${e}'>${e}</span>`
@@ -63,8 +64,22 @@ function popuLateAside() {
     resultsArea.innerHTML = ``
 
     resultWords.forEach((value) => {
-        console.log(value)
-        resultsArea.innerHTML += `<div class='word-aside'>${value}</div>`
+        resultsArea.innerHTML += `<a href='#${value}'><div class='word-aside'>${value}</div></a>`
+    })
+    const descWords = document.querySelectorAll('.word-aside')
+    descWords.forEach((e) => {
+        e.addEventListener('click', () => populateDescription(e.innerText))
+    })
+}
+function populateDescription(arg) {
+    const setDescription = document.querySelector('.description-text')
+    setDescription.innerHTML = ''
+    conflictWords.forEach((e) => {
+        let word = e.word.toLowerCase()
+        let description = e.description
+        if (word == arg.toLowerCase()) {
+            setDescription.innerHTML = `${e.word}: ${description}`
+        }
     })
 }
 
@@ -80,7 +95,7 @@ function onReceiptsSelected() {
 const uploadFile = document.querySelector('#receiptFilesInput')
 uploadFile.addEventListener('change', onReceiptsSelected)
 
-/* Input on press enter */
+/****************************************  Input on press enter *****************************************/
 
 const inputElement = document.querySelector('.words-input')
 
